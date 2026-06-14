04db56c1f884fbcb81281e30f3c48f17914ca3b2
gabzeejnr <gabrieldodowei@gmail.com>
2026-06-14 23:24:42 +0100

    Add selectors for profile card elements

---
+++ scripts/selectors.js
@@ -0,0 +1,17 @@
+export const profileCard = document.querySelector("#profile-card");
+export const overlay = document.querySelector("#overlay");
+
+export const cardHolderName = document.querySelector("#card-holder-name");
+export const cardHolderRole = document.querySelector("#card-holder-role");
+export const cardHolderDepartment = document.querySelector("#card-holder-department");
+
+export const emailLink = document.querySelector(".holder-detail #email a");
+export const phoneLink = document.querySelector(".holder-detail #phone-number a");
+
+export const holderAddress = document.querySelector(".holder-detail #holder-address");
+export const dateJoined = document.querySelector(".holder-detail #date-joined");
+export const aboutText = document.querySelector("#about-holder-text");
+
+export const closeProfileCard = document.querySelector("#close-profile-card");
+
+export const viewButtons = document.querySelectorAll(".view-btn");
